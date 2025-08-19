0c9cc2ec9207663cf51362cf51188a08f97c5b06
Mike <mike.gallagher@live.ie>
2025-08-19 20:55:49 +0300

    asdf

--- RemoveEnglish.py
+++ RemoveEnglish.py
@@ -2,7 +2,7 @@ import os
 from bs4 import BeautifulSoup
 
 # ===== CONFIGURATION =====
-INPUT_FOLDER = r"/Users/mikegallagher/repos/Archive/reading-1/docs/VieSpectrale"
+INPUT_FOLDER = r"/Users/mikegallagher/translate/dailyarticle_output"
 # ========================
 
 def process_html_file(file_path):
